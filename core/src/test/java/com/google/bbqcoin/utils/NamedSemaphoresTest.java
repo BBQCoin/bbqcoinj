7dad9bd545e6908474e6db397de7ba1adac6164f
bigdaub <taylorsminor@gmail.com>
2013-06-29 22:38:57 -0400

    Initial conversion to BBQCoin

---
+++ core/src/test/java/com/google/bbqcoin/utils/NamedSemaphoresTest.java
@@ -0,0 +1,22 @@
+package com.google.bbqcoin.utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bigdaub
+ * Date: 6/28/13
+ * Time: 10:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NamedSemaphoresTest {
+    public void testAcquire() throws Exception {
+
+    }
+
+    public void testTryAcquire() throws Exception {
+
+    }
+
+    public void testRelease() throws Exception {
+
+    }
+}
